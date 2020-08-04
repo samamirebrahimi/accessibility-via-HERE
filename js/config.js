84eb2395091aa95a61bb0e23e08c54eb6a80462b
samamirebrahimi <accipiters@gmail.com>
2020-08-04 13:39:54 +1000

    Added all the files!

---
+++ js/config.js
@@ -0,0 +1,11 @@
+const center = { 
+   lat: -37.831139, 
+   lng: 144.997870, 
+   text: 'Berlin, Germany'
+}
+
+const hereCredentials = {
+   apikey: 'HFeDbmlF5UW8bjf1VyJXgBf-_EnGz3mRxXnZQyqWW8s'
+}
+
+export { center, hereCredentials };
